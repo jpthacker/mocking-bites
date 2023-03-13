719766b5c445767730d52d006997a7dd0e6e83d9
Jack Thacker <jackthacker89@gmail.com>
2023-03-13 12:08:22 +0000

    Add secret_diary integration tests

--- spec/secret_diary_integration_spec.rb
+++ spec/secret_diary_integration_spec.rb
@@ -1,2 +1,26 @@
 require "diary"
 require "secret_diary"
+
+RSpec.describe SecretDiary do
+    it "returns the contents of a diary" do
+        diary = Diary.new("Hello world!")
+        secret_diary = SecretDiary.new(diary)
+        secret_diary.unlock
+        expect(secret_diary.read).to eq "Hello world!"
+    end
+
+    it "initially locks the diary" do
+        diary = Diary.new("Hello World!")
+        secret_diary = SecretDiary.new(diary)
+        expect{ secret_diary.read }.to raise_error "Go away!"
+    end
+
+    it "unlocks and locks the diary" do
+        diary = Diary.new("This works!")
+        secret_diary = SecretDiary.new(diary)
+        secret_diary.unlock
+        expect(secret_diary.read).to eq "This works!"
+        secret_diary.lock
+        expect{ secret_diary.read }.to raise_error "Go away!"
+    end
+end
